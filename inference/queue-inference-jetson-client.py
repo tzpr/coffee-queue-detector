392158fa6a83ef79f3495f5a2fcc2436bdbecc89
Toni <toni.rasanen@iki.fi>
2021-12-18 14:21:25 +0200

    files and instructions

---
+++ inference/queue-inference-jetson-client.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import redis
+import schedule
+import time
+import random
+import jetson.inference
+import jetson.utils
+
+
+# connect to redis
+message_broker = redis.Redis(host='localhost', port=6060)
+
+net = jetson.inference.detectNet('ssd-mobilenet-v2', threshold=0.5)
+camera = jetson.utils.videoSource('/dev/video0')  # V4L2 webcam
+
+
+def analyze_queue_picture():
+    number_of_persons = 0
+    PERSON_CLASS_ID = 1
+    queue_state = 'no queue, no one in the coffee machine'
+
+    img = camera.Capture()
+    detections = net.Detect(img)
+
+    for detection in detections:
+        print(f'DEBUG detection: {detection}')
+        if detection.ClassID == PERSON_CLASS_ID:
+            number_of_persons = number_of_persons + 1
+
+    # https://realpython.com/python-string-formatting/
+    print(f'DEBUG number of persons: {number_of_persons}')
+
+    if number_of_persons > 1:
+        queue_state = f'queue of {number_of_persons} persons'
+
+    if number_of_persons == 1:
+        queue_state = 'no queue, just one person'
+
+    return queue_state
+
+
+def check_queue():
+    queue_state = analyze_queue_picture()
+    print('Current queue state: ', queue_state)
+
+    if message_broker.get('queue_state') is not queue_state:
+        print(f'Update queue state: {queue_state}')
+        message_broker.set('queue_state', queue_state)
+
+
+# schedule scheduler
+# https://pypi.org/project/schedule/
+schedule.every(5).seconds.do(check_queue)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
---
+++ slack-integration/app.js
@@ -0,0 +1,80 @@
+
+const express = require('express'); // https://www.npmjs.com/package/express
+const request = require('request'); // https://www.npmjs.com/package/request
+const redis = require("redis");     // https://github.com/NodeRedis/node_redis
+
+require('dotenv').config()          // https://www.npmjs.com/package/dotenv
+
+
+const redisClient = redis.createClient(process.env.REDIS_PORT, 
+    process.env.REDIS_HOST);
+
+// https://nodejs.org/api/util.html#util_util_promisify_original
+const {promisify} = require('util');
+const redisGetAsync = promisify(redisClient.get).bind(redisClient);
+
+//Check https://api.slack.com/tutorials/tunneling-with-ngrok
+
+// Instantiates Express and assigns app variable to it
+const app = express();
+
+// Define a port we want to listen to
+const PORT = process.env.SLACK_INTEGRATION_API_PORT;
+
+function getQueueState() {
+    return redisGetAsync('queue_state').then(function(res) {
+        console.log('DEBUG redisGetAsync', res);
+	return res;
+    });
+}
+
+redisClient.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+// Lets start server
+app.listen(PORT, function () {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("App listening on port " + PORT);
+});
+
+// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+app.get('/', function(req, res) { 
+    console.log('Hello from space! Ngrok is working!');
+    res.send('Hello from space! Ngrok is working! Path Hit: ' + req.url);
+});
+
+// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+app.get('/oauth', function(req, res) {
+    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+        console.log("Looks like we're not getting code.");
+    } else {
+        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+        request({
+            url: 'https://slack.com/api/oauth.access', //URL to hit
+            qs: {  //Query string data
+                code: req.query.code, 
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET}, 
+            method: 'GET', //Specify the method
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+            }
+        })
+    }
+});
+
+// Route the endpoint that our slash command will point to
+app.post('/command', function(req, res) {
+    // ask queue state from redis
+    getQueueState().then(function(state) {
+        console.log('queue state from redis: ' + state);
+        res.send(state);
+    });
+});
